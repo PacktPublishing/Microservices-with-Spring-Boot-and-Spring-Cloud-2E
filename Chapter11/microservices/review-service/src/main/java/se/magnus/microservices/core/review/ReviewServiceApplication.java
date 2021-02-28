339928e3c68651c9e4067301daea8d51939bf065
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2021-02-28 09:43:13 +0100

    Chapter11

---
+++ Chapter11/microservices/review-service/src/main/java/se/magnus/microservices/core/review/ReviewServiceApplication.java
@@ -0,0 +1,44 @@
+package se.magnus.microservices.core.review;
+
+import java.util.concurrent.Executors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+@SpringBootApplication
+@ComponentScan("se.magnus")
+public class ReviewServiceApplication {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceApplication.class);
+
+  private final Integer connectionPoolSize;
+
+  @Autowired
+  public ReviewServiceApplication(
+    @Value("${spring.datasource.maximum-pool-size:10}")
+    Integer connectionPoolSize
+  ) {
+    this.connectionPoolSize = connectionPoolSize;
+  }
+
+  @Bean
+  public Scheduler jdbcScheduler() {
+    LOG.info("Creates a jdbcScheduler with connectionPoolSize = " + connectionPoolSize);
+    return Schedulers.fromExecutor(Executors.newFixedThreadPool(connectionPoolSize));
+  }
+
+  public static void main(String[] args) {
+    ConfigurableApplicationContext ctx = SpringApplication.run(ReviewServiceApplication.class, args);
+
+    String mysqlUri = ctx.getEnvironment().getProperty("spring.datasource.url");
+    LOG.info("Connected to MySQL: " + mysqlUri);
+  }
+}
---
+++ Chapter11/spring-cloud/gateway/src/test/java/se/magnus/springcloud/gateway/GatewayApplicationTests.java
@@ -0,0 +1,19 @@
+package se.magnus.springcloud.gateway;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+  webEnvironment = RANDOM_PORT,
+  properties = {
+    "spring.security.oauth2.resourceserver.jwt.jwk-set-uri=some-url",
+    "eureka.client.enabled=false"})
+public class GatewayApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
