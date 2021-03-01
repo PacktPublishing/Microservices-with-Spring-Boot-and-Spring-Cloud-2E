70d7dc7f86513eccb27a068b00a2ef1e42905e7d
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2021-03-01 08:30:32 +0100

    Chapter11

--- Chapter11/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/OpenApiConfig.java
+++ Chapter11/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/OpenApiConfig.java
@@ -10,10 +10,13 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
   name = "security_auth", type = SecuritySchemeType.OAUTH2,
   flows = @OAuthFlows(
     authorizationCode = @OAuthFlow(
-    authorizationUrl = "${springdoc.oAuthFlow.authorizationUrl}",
-    tokenUrl = "${springdoc.oAuthFlow.tokenUrl}", scopes = {
-      @OAuthScope(name = "product:read", description = "read scope"),
-      @OAuthScope(name = "product:write", description = "write scope")
-    }
-  )))
+      authorizationUrl = "${springdoc.oAuthFlow.authorizationUrl}",
+      tokenUrl = "${springdoc.oAuthFlow.tokenUrl}",
+      scopes = {
+        @OAuthScope(name = "product:read", description = "read scope"),
+        @OAuthScope(name = "product:write", description = "write scope")
+      }
+    )
+  )
+)
 public class OpenApiConfig {}
