f4a3b3d9b58ecdab58dd63e0496f99ef14dbe88c
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2021-03-09 16:45:49 +0100

    Chapter04

--- Chapter04/microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
+++ Chapter04/microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import se.magnus.api.core.review.Review;
 import se.magnus.api.core.review.ReviewService;
-import se.magnus.util.exceptions.InvalidInputException;
+import se.magnus.api.exceptions.InvalidInputException;
 import se.magnus.util.http.ServiceUtil;
 
 @RestController
