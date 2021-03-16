7b9ac436aeb38d89809fe58e1e7a24d295a46e37
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2021-03-16 11:54:53 +0100

    Chapter14

---
+++ Chapter14/spring-cloud/config-server/src/main/java/se/magnus/springcloud/configserver/SecurityConfig.java
@@ -0,0 +1,23 @@
+package se.magnus.springcloud.configserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      // Disable CRCF to allow POST to /encrypt and /decrypt endpoins
+      .csrf()
+        .disable()
+      .authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+  }
+}
+
+
