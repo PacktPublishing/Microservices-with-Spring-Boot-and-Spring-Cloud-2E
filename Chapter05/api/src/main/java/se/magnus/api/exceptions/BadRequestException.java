13f919dbc648605d2d8911473145fb6ff109d31e
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2021-03-16 09:37:11 +0100

    Chapter05

---
+++ Chapter05/api/src/main/java/se/magnus/api/exceptions/BadRequestException.java
@@ -0,0 +1,17 @@
+package se.magnus.api.exceptions;
+
+public class BadRequestException extends RuntimeException {
+  public BadRequestException() {}
+
+  public BadRequestException(String message) {
+    super(message);
+  }
+
+  public BadRequestException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public BadRequestException(Throwable cause) {
+    super(cause);
+  }
+}
