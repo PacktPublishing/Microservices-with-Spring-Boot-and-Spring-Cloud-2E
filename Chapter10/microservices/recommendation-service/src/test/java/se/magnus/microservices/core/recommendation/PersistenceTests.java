934ed79954fa97630d019f35edb001db154fea61
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2021-04-10 12:07:01 +0200

    Chapter10

--- Chapter10/microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/PersistenceTests.java
+++ Chapter10/microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/PersistenceTests.java
@@ -8,14 +8,15 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.OptimisticLockingFailureException;
 import se.magnus.microservices.core.recommendation.persistence.RecommendationEntity;
 import se.magnus.microservices.core.recommendation.persistence.RecommendationRepository;
 
-@DataMongoTest
-class PersistenceTests {
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+class PersistenceTests extends MongoDbTestBase {
 
   @Autowired
   private RecommendationRepository repository;
