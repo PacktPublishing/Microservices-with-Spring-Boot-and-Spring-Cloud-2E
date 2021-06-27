77415615175b9c3d3f9f4fa71cfed993e447898d
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2021-06-27 13:31:00 +0200

    Chapter10

--- Chapter10/test-em-all.bash
+++ Chapter10/test-em-all.bash
@@ -224,7 +224,7 @@ assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
 
 # Verify access to Swagger and OpenAPI URLs
 echo "Swagger/OpenAPI tests"
-assertCurl 307 "curl -s  http://$HOST:$PORT/openapi/swagger-ui.html"
+assertCurl 302 "curl -s  http://$HOST:$PORT/openapi/swagger-ui.html"
 assertCurl 200 "curl -sL http://$HOST:$PORT/openapi/swagger-ui.html"
 assertCurl 200 "curl -s  http://$HOST:$PORT/openapi/webjars/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config"
 assertCurl 200 "curl -s  http://$HOST:$PORT/openapi/v3/api-docs"
